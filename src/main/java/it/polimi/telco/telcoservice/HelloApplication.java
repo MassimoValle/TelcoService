aac76334424817ea3198a21e26482ff510e484d1
Massimo Valle <32575607+MassimoValle@users.noreply.github.com>
2021-11-19 12:04:43 +0100

    code clean up

--- src/main/java/it/polimi/telco/telcoservice/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.telco.telcoservice;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
