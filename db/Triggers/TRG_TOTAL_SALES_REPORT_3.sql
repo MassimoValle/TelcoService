2df43b5ad3e3e5d203ffc54e5c95c8a8ebf3be1c
Massimo Valle <32575607+MassimoValle@users.noreply.github.com>
2022-01-20 22:34:22 +0100

    3rd trigger fixed

--- db/Triggers/TRG_TOTAL_SALES_REPORT_3.sql
+++ db/Triggers/TRG_TOTAL_SALES_REPORT_3.sql
@@ -12,12 +12,12 @@ AFTER INSERT ON db_Telco_DB2.Order
 FOR EACH ROW
 BEGIN
    	REPLACE INTO db_Telco_DB2.Sales_Report_3
-  		SELECT DISTINCT ServicePackageID, SUM(TotalPrice), 
-						SUM(TotalPrice-PeriodID * (SELECT COALESCE(SUM(MonthlyFee),0)
+  		SELECT DISTINCT SUB.ServicePackageID, SUM(TotalPrice), SUM(TotalPrice - PeriodID*
+						(SELECT COALESCE(SUM(MonthlyFee),0)
    						FROM db_Telco_DB2.Product AS P JOIN db_Telco_DB2.SOP AS S
-						WHERE S.OptionalProductID = P.ID AND S.SubscriptionID = Subscription.ID
-						GROUP BY SubscriptionID))
-    		FROM db_Telco_DB2.Subscription
-    		GROUP BY ServicePackageID;
+						WHERE S.OptionalProductID = P.ID AND S.SubscriptionID = SUB.ID
+                        ))
+		FROM Subscription AS SUB
+		GROUP BY ServicePackageID;
 END;//
 DELIMITER ;
